79dd9a7be187372eeab63d63f3636e385ca0b977
AVirolainen <58146499+AVirolainen@users.noreply.github.com>
2020-06-10 21:52:45 +0300

    Update Shield.java

--- Lab6/src/com/example/lab6/Shield.java
+++ Lab6/src/com/example/lab6/Shield.java
@@ -2,7 +2,8 @@ package com.example.lab6;
 
 public class Shield extends Armor {
 
-    public Shield(){
-        super("Dragon Shield", 10, 100, 120);
+    public Shield(String name1, int v, int w, int p){
+        super(name1, v, w, p);
     }
 }
+
